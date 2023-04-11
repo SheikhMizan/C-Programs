eae91946fdb1cdd46c8ebfc734fc79489bd4a1be
Sheikh MiZan <sheikhmizanbd@gmail.com>
2023-04-11 15:56:38 +0600

    Update and rename Problems0.cpp to Solution1-7.cpp

--- Solution1-7.cpp
+++ Solution1-7.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 int main() {
- // area of rectangle
+   // 1. area of rectangle
    
     int l,w,a;
     cin >> l;
@@ -11,14 +11,14 @@ int main() {
 
 
 
-    // Volume of a sphere
+    // 2. Volume of a sphere
     float r, v;
     cin >> r;
     v = (4*3.14*r*r*r)/3;
     cout << "The volume of sphere is = " << v << endl;
     return 0;
 
-    // conversion of Fahrenheit to centrigrade
+    // 3.  conversion of Fahrenheit to centrigrade
     float F, C;
     cout << "Enter the temperature in Fahrenheit\n";
     cin >> F;
@@ -27,7 +27,7 @@ int main() {
         << C <<" Centigrade";
 
 
-    // average of 3 numbers
+    // 4. average of 3 numbers
     int n1,n2,n3;
     cin >> n1 >> n2 >> n3;
     float avg = (n1+n2+n3) / 3;
@@ -36,44 +36,37 @@ int main() {
 
 
 
-
+      // 5. BMI Calculator
     float h, w, BMI;
     cin >> h >> w;
     BMI = (w/1000) / (h*h);
     cout << "Body Mass = " << BMI << endl;
 
 
-
+    // 6 program that asks the user to enter their current weight, their goal weight, and the number of weeks they have to reach their goal, and then calculates and displays the number of pounds they need to lose per week.
     float crntweight, goal, weeks;
     cout << "Please input your current weight and then your goal weight followed by number of week to reach your goal" << endl;
     cin >> crntweight >> goal >> weeks;
     cout << "Number of pounds to lose per week to reach the goal " << ( crntweight - goal ) / weeks << endl;
 
 
-    // Get user input
+  // 7. program that asks the user to enter the amount of money they want to save and the interest rate (as a percentage), and then calculates and displays the number of years it will take to reach their savings goal.
+    
     float savings_goal, interest_rate;
     cout << "Enter your savings goal: $";
     cin >> savings_goal;
     cout << "Enter the interest rate (%): ";
     cin >> interest_rate;
-
-    // Convert interest rate to decimal form
     interest_rate /= 100.0;
-
-    // Initialize variables
     int years = 0;
     float savings_balance = 0;
-
-    // Calculate the number of years required to reach the savings goal
     while (savings_balance < savings_goal) {
         float savings_for_year;
-        cout << "How much money will you save this year? $";
+        cout << "How much money will you save for year " <<  years << endl;
         cin >> savings_for_year;
         savings_balance += savings_balance * interest_rate;
         savings_balance += savings_for_year;
         years++;
     }
-
-    // Print the result
     cout << "It will take " << years << " years to reach your savings goal of $" << savings_goal << endl;
 }
